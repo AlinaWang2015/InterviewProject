b3cafb52c700775310b374e0ff0100c48b3158c2
Alina <13834610906@163.com>
2020-01-21 19:11:31 +0800

    [FixBug] 小明随机数去重的问题

--- 华为机试/delete_duplication_num.py
+++ 华为机试/delete_duplication_num.py
@@ -1,18 +1,25 @@
-def delete_duplication(num_count):
-    num_arr = []
-    for i in range(num_count):
-        num_arr.append(int(input()))
-
-    num_set = set(num_arr)
-
-    num_lis = list(num_set)
-    num_lis.sort()
-    for num in num_lis:
-        print(num)
+# 输入描述:
+# 输入多行，先输入随机整数的个数，再输入相应个数的整数
+#
+# 输出描述:
+# 返回多行，处理后的结果
 
 
-if __name__ == '__main__':
-    num_count = int(input())
-    if num_count <= 0:
-        exit()
-    delete_duplication(num_count)
+import sys
+
+while True:
+    try:
+        num_count = int(input())
+        num_arr = []
+        for i in range(0, num_count):
+            num_arr.append(int(sys.stdin.readline()))
+
+        num_set = set(num_arr)
+        num_lis = list(num_set)
+        sorted(num_lis)
+
+        for num in num_lis:
+            print(num)
+    except:
+        break
+
