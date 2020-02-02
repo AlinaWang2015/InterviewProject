41320b90cf5edf43d86489cb3a9d84df790f8c27
Alina <13834610906@163.com>
2020-02-02 18:50:36 +0800

    [Update]分割字符串，不足0替补

---
+++ 华为机试/String_splitting.py
@@ -0,0 +1,18 @@
+# •连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+# •长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
+
+
+import sys
+
+a = sys.stdin.readline().replace('\n', '')
+b = sys.stdin.readline().replace('\n', '')
+c = [a[i:i + 8] for i in range(0, len(a), 8)]
+d = [b[i:i + 8] for i in range(0, len(b), 8)]
+z = c + d
+for i in z:
+    if len(i) < 8:
+        x = i + (8 - len(i)) * '0'
+        print(x)
+    else:
+        print(i)
+
