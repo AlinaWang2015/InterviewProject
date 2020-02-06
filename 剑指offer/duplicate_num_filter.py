7ca29056631d5a72df5136d16bf5f5292e9c69fd
Alina <13834610906@163.com>
2020-02-06 19:42:15 +0800

    [Update]3.数组中重复的数字

--- 剑指offer/duplicate_num_filter.py
+++ 剑指offer/duplicate_num_filter.py
@@ -4,4 +4,56 @@
 
 # 解决办法一：先排序，然后排序后的数组中，从头到尾扫描一遍数组就可以了。但是这样需要的时间复杂度是o(nlogn)
 
-# 解决办法二：
+# 解决办法二：使用set结构或hash结构
+
+
+def use_map(numbers):
+    """
+    使用哈希表结构
+    :param numbers:
+    :return:
+    """
+    num_map = dict()
+    for number in numbers:
+        if number in num_map:
+            print(number)
+        else:
+            num_map[number] = True
+
+
+def use_set(numbers):
+    """
+    使用set结构
+    :param numbers:
+    :return:
+    """
+    num_set = set()
+    for number in numbers:
+        if number in num_set:
+            print(number)
+        else:
+            num_set.add(number)
+
+
+def use_sort(numbers):
+    """
+    排序后查找
+    :param numbers:
+    :return:
+    """
+    numbers = sorted(numbers)
+    print(numbers)
+    for index, number in enumerate(numbers):
+        if index != number:
+            continue
+        if number == numbers[number]:
+            print(number)
+        else:
+            numbers[index], numbers[number] = numbers[number], numbers[index]
+
+
+if __name__ == '__main__':
+    numbers = [2, 3, 1, 0, 2, 5, 3]
+    use_map(numbers)
+    use_set(numbers)
+    use_sort(numbers)
